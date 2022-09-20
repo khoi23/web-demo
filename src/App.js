5923ccca1693ec9ebb0421ae4f074c65c40e4600
khoi23 <101267249+khoi23@users.noreply.github.com>
2022-09-21 02:30:30 +0700

    video 48

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import Header from './Components/Header/Header'
+import Header from './Components/Header/Header';
+import { Link } from "react-router-dom";
+
 
 const App = () => {
   return (
-    <div className='app-container'>
+    <div className="app-container">
       <Header />
+      <div>
+        Test Link
+        <div>
+          <button>
+            <Link to="/users">go to user page</Link>
+          </button>
+          <button>
+            <Link to="/admins">go to admin page</Link>
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import User from './Components/User/User';
+import Admin from "./Components/Admin/Admin";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="users" element={<User />} />
+      <Route path="admins" element={<Admin />} />
+    </Routes>
+  </BrowserRouter>
 );
